3a7e54c0eabfc8242d66ccf8cdb564a328c54bea
Benjamin Villa <benjamin.j.villa@gmail.com>
2022-04-05 14:47:30 -0400

    Set up basic React App. Created sub folders for organization. Deleted not needed files from initial npx. Installed React Router and added tags to index/App for future use.

---
+++ src/App/App.js
@@ -0,0 +1,31 @@
+// App imports
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// App function
+export default function App() {
+
+  return (
+    <main className="App">
+      <header>
+
+      </ header>
+      
+      <content>
+        <Routes>
+          <Route />
+
+        </ Routes>
+
+      </ content>
+
+      <footer>
+
+      </ footer>
+      
+    </ main>
+  );
+}
+
+
