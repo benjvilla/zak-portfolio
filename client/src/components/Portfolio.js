4f2de53e892dc5e140155c0265de27b5abb409de
jmaille664 <juliamaille@gmail.com>
2022-04-11 14:01:02 -0400

    updated cards and accordian

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,17 +1,9 @@
 import React from "react";
 import {
-  Button,
-  CardColumns,
   Card,
-  CardTitle,
-  CardSubtitle,
-  CardImg,
-  CardBody,
-  CardText,
   Accordion,
-  AccordionItem,
-  AccordionHeader,
-  Collapse,
+  Col,
+  Row,
   Container
 
 } from "react-bootstrap";
@@ -21,28 +13,89 @@ import NEOWeek1 from "../images/New Hire Orientation - Week 1.jpg";
 export default function Portfolio() {
 
   return (
-    <>
-    <Container>
+    <><Container fluid="md">
     <div className="container">
   <div className="row">
     <div className="col">
-      {/* Card  */}
-      <Card style={{ width: '25rem' }}>
+      {/* Card 1 */}
+      <Card style={{ width: '50rem',
+     }}>
   <Card.Img variant="top" src={NEOWeek1}
   
   />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Card Title 1</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Accordion defaultActiveKey="0">
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>Accordion Item #1</Accordion.Header>
+    <Accordion.Body>
+    <Card.Img variant="top" src={NEOWeek1}
+  />
+    </Accordion.Body>
+  </Accordion.Item>
+  </Accordion>
   </Card.Body>
 </Card>
+
+{/* Card 2 */}
+ <Card style={{ width: '50rem' }}>
+  <Card.Img variant="top" src={NEOWeek1}
+  
+  />
+  <Card.Body>
+    <Card.Title>Card Title 2</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Accordion defaultActiveKey="0">
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>Accordion Item #2</Accordion.Header>
+    <Accordion.Body>
+    <Card.Img variant="top" src={NEOWeek1}
+  />
+    </Accordion.Body>
+  </Accordion.Item>
+  </Accordion>
+  </Card.Body>
+</Card>
+{/* Card 3 */}
+<Card style={{ width: '50rem' }}>
+  <Card.Img variant="top" src={NEOWeek1}
+  
+  />
+  <Card.Body>
+    <Card.Title>Card Title 3</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Accordion defaultActiveKey="0">
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>Accordion Item #3</Accordion.Header>
+    <Accordion.Body>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Accordion.Body>
+  </Accordion.Item>
+  </Accordion>
+  </Card.Body>
+</Card>
+{/* card 4  */}
+
       </div>
       </div>
       </div>
+
 </Container>
     </>
   );
