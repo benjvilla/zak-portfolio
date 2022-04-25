8e5ab766a240ca275bb41553765cc625356b9993
jmaille664 <juliamaille@gmail.com>
2022-04-25 10:59:30 -0400

    Updated schema

--- ServerSchema.js
+++ ServerSchema.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const ServerSchema = new mongoose.Schema({
     //key value pairs are the name of the key and what TYPE you want the value to be
     date: Date,
+    name: String,
     email: String,
     subject: String,
     message: String,
