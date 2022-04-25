302b8b0c985ef98a622ef4a32f69c7fd6e14a52c
Leah N-C <Lniemcav@gmail.com>
2022-04-25 15:12:21 -0400

    Changed fetch path in ContactForm -- Fourth deploy attempt

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -49,7 +49,7 @@ export default function ContactForm() {
     };
 
     // Fetch from server
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://zak-portfolio-capstone.herokuapp.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
